75cb18790276172a95cfd324a92eced488069d94
sonuprasad66 <sp.srp1999@gmail.com>
2023-01-08 00:07:02 +0530

    b

--- backend/index.js
+++ backend/index.js
@@ -16,9 +16,9 @@ app.use("/", userRouter);
 app.listen(process.env.PORT, async () => {
   try {
     await connection;
-    console.log("Database connection successfull");
+    console.log("Database connected successfull");
   } catch (err) {
-    console.log("Error to connect mongo database");
+    console.log("Database connected failed");
     console.log(err);
   }
   console.log(`App listening on port ${process.env.PORT}`);
